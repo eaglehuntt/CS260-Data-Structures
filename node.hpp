80be707e25eb991b53a9aef81f390c7fea2dc234
Eagle Hunt <eagle@eaglehunt.net>
2024-02-14 00:33:50 -0800

    updated node

--- node.hpp
+++ node.hpp
@@ -17,15 +17,15 @@ class Node {
         void set_value(int new_value);
         void set_index(int new_index);
         void new_value(int new_value);
-        void set_next(Node* new_next);
-        void set_previous(Node* new_prev);
+        void set_next(Node *new_next);
+        void set_previous(Node *new_prev);
 
         // Wrapper methods for set_next & set_previous
-        void set_right(Node* new_right);
-        void set_left(Node* new_left);
+        void set_right(Node *new_right);
+        void set_left(Node *new_left);
 
         int get_value();
         int get_index();
-        Node* get_next();
-        Node* get_previous();
+        Node *get_next();
+        Node *get_previous();
 };
